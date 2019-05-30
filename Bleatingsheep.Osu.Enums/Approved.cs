2fc042c945aa7f130f852ae8ed5f2a5923caee83
bleatingsheep <bleatingsheep@live.cn>
2019-05-30 23:40:50 +0800

    Add Approved.

---
+++ Bleatingsheep.Osu.Enums/Approved.cs
@@ -0,0 +1,13 @@
+﻿namespace Bleatingsheep.Osu
+{
+    public enum Approved
+    {
+        Loved = 4,
+        Qualified = 3,
+        Approved = 2,
+        Ranked = 1,
+        Pending = 0,
+        Wip = -1,
+        Graveryard = -2,
+    }
+}
